fcdf1b22f1ee0330a8779f0fabb243e2624e4463
Gilang Eka Pratama <gilangpratama20997@gmail.com>
2024-11-19 10:45:10 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,15 @@ Route::get('/', function () {
 });
 
 //product
-Route::get('product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 
 //penjualan
-Route::get('penjualan/{sales_id}/edit', [PenjualanController::class, 'edit'])->name('penjualan.edit');
-Route::get('penjualan', [PenjualanController::class, 'index'])->name('penjualan.index');
-Route::get('penjualan/create', [PenjualanController::class, 'create'])->name('penjualan.create');
-Route::get('product/{product_id}', [ProductController::class, 'getPriceById'])->name('product.getPriceById');
-Route::post('penjualan', [PenjualanController::class, 'store'])->name('penjualan.store');
+Route::get('/penjualan/{sales_id}/edit', [PenjualanController::class, 'edit'])->name('penjualan.edit');
+Route::get('/penjualan', [PenjualanController::class, 'index'])->name('penjualan.index');
+Route::get('/penjualan/create', [PenjualanController::class, 'create'])->name('penjualan.create');
+Route::get('/product/{product_id}', [ProductController::class, 'getPriceById'])->name('product.getPriceById');
+Route::post('/penjualan', [PenjualanController::class, 'store'])->name('penjualan.store');
 
-Route::put('penjualan/{sales_id}', [PenjualanController::class, 'update'])->name('penjualan.update');
-Route::delete('penjualan/{sales_id}', [PenjualanController::class, 'destroy'])->name('penjualan.destroy');
+Route::put('/penjualan/{sales_id}', [PenjualanController::class, 'update'])->name('penjualan.update');
+Route::delete('/penjualan/{sales_id}', [PenjualanController::class, 'destroy'])->name('penjualan.destroy');
 
